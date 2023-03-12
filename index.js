42e61284d5b5f421a6a0b53ce063c1567319d2ea
Amit Raghuwanshi <93573017+raghuwanshi25@users.noreply.github.com>
2023-03-12 17:32:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1= Math.floor(Math.random()*6)+1;
+var randomDiceImage= "dice"+randomNumber1+".png";
+var randomDiceSource="images/"+randomDiceImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceSource);
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomDiceSource2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 wins👑";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 wins👑";
+}
+else if(randomNumber1===randomNumber2){
+    document.querySelector("h1").innerHTML="Draw";
+
+}
